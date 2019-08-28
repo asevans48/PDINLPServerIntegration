bbaa12a4f6669712658cf9e4576419820f5563f4
Andrew Evans <asevans48@gmail.com>
2019-08-28 01:34:17 -0600

    initial commit

---
+++ src/test/java/com/si/TestJavaCeleryIntegration.java
@@ -0,0 +1,19 @@
+/**
+ * Tests integration with celery-java. Specifict tests are on celery-java.
+ *
+ * Must have a running instance of NLP Server configured for tests to work. Intentionally not
+ * working here.
+ *
+ */
+package com.si;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestJavaCeleryIntegration {
+
+    @Test
+    public void shouldIntegrateWithRunningCeleryApplication(){
+
+    }
+}
